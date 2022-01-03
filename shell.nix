4d937cbc276c7a92acb8474bb91371e0ad34826b
Tom Gustafsson <tom.gustafsson@iki.fi>
2022-01-03 09:25:16 +0200

    Add shell.nix for development

---
+++ shell.nix
@@ -0,0 +1,28 @@
+with import <nixpkgs> {};
+
+(let
+  pacopy016 = python38.pkgs.buildPythonPackage rec {
+    # not part of nixpkgs
+    pname = "pacopy";
+    version = "0.1.6";
+    format = "pyproject";
+    src = python38.pkgs.fetchPypi {
+      inherit pname version;
+      sha256 = "0rq5yfmq5516giyqsflm3bjirbfhigydd1vlxc554jnn13n3wisr";
+    };
+    doCheck = false;
+    meta = {
+      homepage = "https://github.com/nschloe/pacopy/";
+      description = "Numerical continuation in Python";
+    };
+  };
+in python38.withPackages (ps: with ps; [
+  ipython
+  pytest
+  flask
+  werkzeug
+  itsdangerous
+  colorama
+  markupsafe
+  jinja2
+])).env
